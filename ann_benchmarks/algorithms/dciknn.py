018ebf29f2647affa1be296efde1b98c1b991e27
Craig Gross <craig@c-gross.net>
2022-02-08 23:43:28 -0500

    Fix dciknn wrapper

--- ann_benchmarks/algorithms/dciknn.py
+++ ann_benchmarks/algorithms/dciknn.py
@@ -12,7 +12,6 @@ class DCIKNN(BaseANN):
         self.name = 'DCIKNN(nci=%d, nsi=%d, nl=%d, cfov=%d, cptr=%f)' % (
             num_comp_indices, num_simp_indices, num_levels,
             construction_field_of_view, construction_prop_to_retrieve)
-        self._metric = metric
         self._num_levels = num_levels
         self._num_comp_indices = num_comp_indices
         self._num_simp_indices = num_simp_indices
@@ -28,13 +27,13 @@ class DCIKNN(BaseANN):
                            self._num_simp_indices)
 
     def fit(self, X):
-        self._dci_db.add(X, num_levels=self._numlevels,
+        self._dci_db.add(X, num_levels=self._num_levels,
                          field_of_view=self._construction_field_of_view,
                          prop_to_retrieve=self._construction_prop_to_retrieve)
 
     def set_query_arguments(self, query_field_of_view, query_prop_to_retrieve):
         self._query_field_of_view = query_field_of_view
-        self._query_field_of_view = query_field_of_view
+        self._query_prop_to_retrieve = query_prop_to_retrieve
 
     def query(self, v, n):
         return self._dci_db.query(np.array([v]), num_neighbours=n,

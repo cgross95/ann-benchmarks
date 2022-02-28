f9b33aefee5a388e438987565f2e5327592f3a4a
Craig Gross <craig@c-gross.net>
2022-02-28 18:35:19 -0500

    Add preliminary support for pydci algorithm

---
+++ ann_benchmarks/algorithms/pydci.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import
+import numpy as np
+from pydci import DCI
+from ann_benchmarks.algorithms.base import BaseANN
+
+
+class PYDCIKNN(BaseANN):
+    def __init__(self, dim, num_simple=10, num_composite=10):
+        self.name = 'PYDCIKNN(d=%d, num_simple=%d, num_composite=%d' % (
+            dim, num_simple, num_composite)
+        self._dim = dim
+        self._num_simple = num_simple
+        self._num_composite = num_composite
+
+        # query arguments
+        self._max_retrieve = None
+        self._max_composite_visit = None
+
+        # set up empty database
+        self._dci = DCI(dim, num_simple, num_composite)
+        self._fitted = False
+
+    def fit(self, X):
+        # TODO: Can support dynamic refitting
+        if self._fitted:
+            self._dci = DCI(self._dim, self._num_simple, self._num_composite,
+                            X)
+        self._dci.add(X)
+        self._fitted = True
+
+    def set_query_arguments(self, max_retrieve, max_composite_visit):
+        self._max_retrieve = max_retrieve
+        self._max_composite_visit = max_composite_visit
+
+    def query(self, v, n):
+        # TODO: This gives back the points, not the indices
+        return self._dci.query(np.array([v]), k=n,
+                               max_retrieve=self._max_retrieve,
+                               max_composite_visit=self._max_composite_visit,
+                               )

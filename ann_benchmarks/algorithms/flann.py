49d32ca36f3877b501ce0cb8b2de1e0564173ae1
Craig Gross <craig@c-gross.net>
2022-04-08 13:36:24 -0400

    Add dynamic updating for flann

--- ann_benchmarks/algorithms/flann.py
+++ ann_benchmarks/algorithms/flann.py
@@ -10,18 +10,22 @@ class FLANN(BaseANN):
         self._target_precision = target_precision
         self.name = 'FLANN(target_precision=%f)' % self._target_precision
         self._metric = metric
+        print(self.name)
 
     def fit(self, X):
         self._flann = pyflann.FLANN(
             target_precision=self._target_precision,
-            algorithm='autotuned', log_level='info')
+            algorithm='composite', log_level='info')
         if self._metric == 'angular':
             X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
         self._flann.build_index(X)
 
+    def update(self, X):
+        self._flann.add_points(X)
+
     def query(self, v, n):
         if self._metric == 'angular':
             v = sklearn.preprocessing.normalize([v], axis=1, norm='l2')[0]
-        if v.dtype != numpy.float32:
-            v = v.astype(numpy.float32)
+        # if v.dtype != numpy.float32:
+        #     v = v.astype(numpy.float32)
         return self._flann.nn_index(v, n)[0][0]

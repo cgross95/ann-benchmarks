255a60badbe2444f2a83a5d25b9848443d1cb750
Craig Gross <craig@c-gross.net>
2022-04-11 00:12:07 -0400

    Add algorithm parameter to flann

--- ann_benchmarks/algorithms/flann.py
+++ ann_benchmarks/algorithms/flann.py
@@ -6,19 +6,20 @@ from ann_benchmarks.algorithms.base import BaseANN
 
 
 class FLANN(BaseANN):
-    def __init__(self, metric, target_precision):
+    def __init__(self, metric, algorithm, target_precision):
         self._target_precision = target_precision
         self.name = 'FLANN(target_precision=%f)' % self._target_precision
         self._metric = metric
+        self._algorithm = algorithm
         print(self.name)
 
     def fit(self, X):
         self._flann = pyflann.FLANN(
             target_precision=self._target_precision,
-            algorithm='composite', log_level='info')
+            algorithm=self._algorithm, log_level='info')
         if self._metric == 'angular':
             X = sklearn.preprocessing.normalize(X, axis=1, norm='l2')
-        self._flann.build_index(X)
+        print(self._flann.build_index(X))
 
     def update(self, X):
         self._flann.add_points(X)

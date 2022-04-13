e03f9b5fece299b4f0cc7157c7d06026506d9e40
Craig Gross <craig@c-gross.net>
2022-04-13 11:59:42 -0400

    Generalize plotting script

--- plot_script.sh
+++ plot_script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
- echo "bruteforce flann"
- python plot_dynamic.py --dataset siemens-big-sherpa --best_metric recall total_time --algorithms bruteforce flann --smooth --intervals --landscape
+DATASET=siemens-big-sherpa
+METRICS="recall total_time"
+OPTIONS="--smooth --intervals"
+echo "bruteforce flann"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS
 echo "bruteforce hnswlib"
-python plot_dynamic.py --dataset siemens-big-sherpa --best_metric recall total_time --algorithms bruteforce hnswlib --smooth --intervals --landscape
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce hnswlib $OPTIONS
 echo "bruteforce dciknn pydci"
-python plot_dynamic.py --dataset siemens-big-sherpa --best_metric recall total_time --algorithms bruteforce dciknn pydci --smooth --intervals --landscape
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn pydci $OPTIONS
 echo "bruteforce dciknn flann hnswlib pydci sptag"
-python plot_dynamic.py --dataset siemens-big-sherpa --best_metric recall total_time --algorithms bruteforce dciknn flann hnswlib pydci sptag --smooth --intervals --landscape
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn flann hnswlib pydci sptag $OPTIONS
 echo "bruteforce flann hnswlib sptag"
-python plot_dynamic.py --dataset siemens-big-sherpa --best_metric recall total_time --algorithms bruteforce flann hnswlib sptag --smooth --intervals --landscape
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann hnswlib sptag $OPTIONS

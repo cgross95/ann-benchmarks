c8d32cacc0b70c82b2615dd5dd70c1ed35abfe06
Craig Gross <craig@c-gross.net>
2022-04-28 11:52:48 -0400

    Enable all runs, plots, and parameters in scripts

--- plot_script.sh
+++ plot_script.sh
@@ -1,129 +1,132 @@
 #!/bin/bash
 METRICS="recall total_time elapsed" 
-OPTIONS="--smooth 10 --landscape --font 20"
 
 ### BIG SHERPA ###
 
-# DATASET=siemens-big-sherpa
-# 
-# # ALL
-# echo "bruteforce dciknn flann hnswlib pydci sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn flann hnswlib pydci sptag $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
-# 
-# # DCI ONLY
-# echo "bruteforce dciknn pydci"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn pydci $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
-# 
-# # NON-DCI ONLY
-# echo "bruteforce flann hnswlib sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann hnswlib sptag $OPTIONS --build_lim -0.01 0.2 --search_lim -0.001 0.02 --total_lim -0.01 0.2
-# 
-# # HNSW
-# echo "bruteforce hnswlib"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce hnswlib $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
-# 
-# # SMOOTH FLANN
+DATASET=siemens-big-sherpa
+OPTIONS="--smooth 100 --landscape --font 20"
+
+# ALL
+echo "bruteforce dciknn flann hnswlib pydci sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn flann hnswlib pydci sptag $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
+
+# DCI ONLY
+echo "bruteforce dciknn pydci"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn pydci $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
+
+# NON-DCI ONLY
+echo "bruteforce flann hnswlib sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann hnswlib sptag $OPTIONS --build_lim -0.01 0.2 --search_lim -0.001 0.02 --total_lim -0.01 0.2
+
+# HNSW
+echo "bruteforce hnswlib"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce hnswlib $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
+
+# SMOOTH FLANN
+echo "bruteforce flann"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.01 --search_lim -0.0005 0.007 --total_lim -0.0005 0.011
+# # NONSMOOTH FLANN
 # echo "bruteforce flann"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.01 --search_lim -0.0005 0.007 --total_lim -0.0005 0.011
-# # # NONSMOOTH FLANN
-# # echo "bruteforce flann"
-# # python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.5 --search_lim -0.0005 0.007 --total_lim -0.0005 0.5
-# 
-# # DCIKNN
-# echo "bruteforce dciknn"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
-# 
-# # PYDCI
-# echo "bruteforce sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce pydci $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
-# 
-# # SPTAG
-# echo "bruteforce sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce sptag $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
+# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.5 --search_lim -0.0005 0.007 --total_lim -0.0005 0.5
 
-### SHERPA ###
+# DCIKNN
+echo "bruteforce dciknn"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
 
-# DATASET=siemens-sherpa
-# 
-# # ALL
-# echo "bruteforce dciknn flann hnswlib pydci sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn flann hnswlib pydci sptag $OPTIONS --build_lim -0.01 1 --search_lim 0 5 --total_lim -0.01 5
-# 
-# # DCI ONLY
-# echo "bruteforce dciknn pydci"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn pydci $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
-# 
-# # NON-DCI ONLY
-# echo "bruteforce flann hnswlib sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann hnswlib sptag $OPTIONS --build_lim -0.01 0.2 --search_lim -0.001 0.02 --total_lim -0.01 0.2
-# 
-# # HNSW
-# echo "bruteforce hnswlib"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce hnswlib $OPTIONS --build_lim -0.0005 0.015 --search_lim -0.00005 0.002 --total_lim -0.0005 0.015
-# 
-# # SMOOTH FLANN
+# PYDCI
+echo "bruteforce sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce pydci $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
+
+# SPTAG
+echo "bruteforce sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce sptag $OPTIONS --build_lim -0.00005 0.02 --search_lim -0.0005 0.007 --total_lim -0.00005 0.02
+
+# SHERPA ###
+
+DATASET=siemens-sherpa
+OPTIONS="--smooth 10 --landscape --font 20"
+
+# ALL
+echo "bruteforce dciknn flann hnswlib pydci sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn flann hnswlib pydci sptag $OPTIONS --build_lim -0.01 1 --search_lim 0 5 --total_lim -0.01 5
+
+# DCI ONLY
+echo "bruteforce dciknn pydci"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn pydci $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
+
+# NON-DCI ONLY
+echo "bruteforce flann hnswlib sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann hnswlib sptag $OPTIONS --build_lim -0.01 0.2 --search_lim -0.001 0.02 --total_lim -0.01 0.2
+
+# HNSW
+echo "bruteforce hnswlib"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce hnswlib $OPTIONS --build_lim -0.0005 0.015 --search_lim -0.00005 0.002 --total_lim -0.0005 0.015
+
+# SMOOTH FLANN
+echo "bruteforce flann"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.004 --search_lim -0.00005 0.002 --total_lim -0.0005 0.004
+# # NONSMOOTH FLANN
 # echo "bruteforce flann"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.004 --search_lim -0.00005 0.002 --total_lim -0.0005 0.004
-# # # NONSMOOTH FLANN
-# # echo "bruteforce flann"
-# # python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.5 --search_lim -0.0005 0.007 --total_lim -0.0005 0.5
-# 
-# # DCIKNN
-# echo "bruteforce dciknn"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn $OPTIONS --build_lim -0.00005 0.3 --search_lim -0.00005 0.01 --total_lim -0.00005 0.3 
-# 
-# # PYDCI
-# echo "bruteforce sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce pydci $OPTIONS --build_lim -0.00005 0.004 --search_lim -0.00005 3 --total_lim -0.00005 3 --ratio_lim 0 5
-# 
-# # SPTAG
-# echo "bruteforce sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce sptag $OPTIONS --build_lim -0.00005 0.2  --search_lim -0.00005 0.005 --total_lim -0.00005 0.2
+# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.5 --search_lim -0.0005 0.007 --total_lim -0.0005 0.5
+
+# DCIKNN
+echo "bruteforce dciknn"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn $OPTIONS --build_lim -0.00005 0.3 --search_lim -0.00005 0.01 --total_lim -0.00005 0.3 
+
+# PYDCI
+echo "bruteforce sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce pydci $OPTIONS --build_lim -0.00005 0.004 --search_lim -0.00005 3 --total_lim -0.00005 3 --ratio_lim 0 5
+
+# SPTAG
+echo "bruteforce sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce sptag $OPTIONS --build_lim -0.00005 0.2  --search_lim -0.00005 0.005 --total_lim -0.00005 0.2
 
 
-### OLHC ###
+# OLHC ###
 
-# DATASET=siemens-olhc
-# 
-# # ALL
-# echo "bruteforce dciknn flann hnswlib pydci sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn flann hnswlib pydci sptag $OPTIONS --build_lim -0.01 1 --search_lim 0 5 --total_lim -0.01 5
-# 
-# # DCI ONLY
-# echo "bruteforce dciknn pydci"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn pydci $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
-# 
-# # NON-DCI ONLY
-# echo "bruteforce flann hnswlib sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann hnswlib sptag $OPTIONS --build_lim -0.01 0.2 --search_lim -0.001 0.02 --total_lim -0.01 0.2
-# 
-# # HNSW
-# echo "bruteforce hnswlib"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce hnswlib $OPTIONS --build_lim -0.0005 0.015 --search_lim -0.00005 0.002 --total_lim -0.0005 0.015
-# 
-# # SMOOTH FLANN
+DATASET=siemens-olhc
+OPTIONS="--smooth 10 --landscape --font 20"
+
+# ALL
+echo "bruteforce dciknn flann hnswlib pydci sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn flann hnswlib pydci sptag $OPTIONS --build_lim -0.01 1 --search_lim 0 5 --total_lim -0.01 5
+
+# DCI ONLY
+echo "bruteforce dciknn pydci"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn pydci $OPTIONS --build_lim -0.001 6 --search_lim 0 25 --total_lim -0.001 25
+
+# NON-DCI ONLY
+echo "bruteforce flann hnswlib sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann hnswlib sptag $OPTIONS --build_lim -0.01 0.2 --search_lim -0.001 0.02 --total_lim -0.01 0.2
+
+# HNSW
+echo "bruteforce hnswlib"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce hnswlib $OPTIONS --build_lim -0.0005 0.015 --search_lim -0.00005 0.002 --total_lim -0.0005 0.015
+
+# SMOOTH FLANN
+echo "bruteforce flann"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.004 --search_lim -0.00005 0.002 --total_lim -0.0005 0.004
+# # NONSMOOTH FLANN
 # echo "bruteforce flann"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.004 --search_lim -0.00005 0.002 --total_lim -0.0005 0.004
-# # # NONSMOOTH FLANN
-# # echo "bruteforce flann"
-# # python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.5 --search_lim -0.0005 0.007 --total_lim -0.0005 0.5
-# 
-# # DCIKNN
-# echo "bruteforce dciknn"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn $OPTIONS --build_lim -0.00005 0.3 --search_lim -0.00005 0.01 --total_lim -0.00005 0.3 
-# 
-# # PYDCI
-# echo "bruteforce sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce pydci $OPTIONS --build_lim -0.00005 0.004 --search_lim -0.00005 3 --total_lim -0.00005 3 --ratio_lim 0 5
-# 
-# # SPTAG
-# echo "bruteforce sptag"
-# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce sptag $OPTIONS --build_lim -0.00005 0.2  --search_lim -0.00005 0.005 --total_lim -0.00005 0.2
+# python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce flann $OPTIONS --build_lim -0.0005 0.5 --search_lim -0.0005 0.007 --total_lim -0.0005 0.5
+
+# DCIKNN
+echo "bruteforce dciknn"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce dciknn $OPTIONS --build_lim -0.00005 0.3 --search_lim -0.00005 0.01 --total_lim -0.00005 0.3 
+
+# PYDCI
+echo "bruteforce sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce pydci $OPTIONS --build_lim -0.00005 0.004 --search_lim -0.00005 3 --total_lim -0.00005 3 --ratio_lim 0 5
+
+# SPTAG
+echo "bruteforce sptag"
+python plot_dynamic.py --dataset $DATASET --best_metric $METRICS --algorithms bruteforce sptag $OPTIONS --build_lim -0.00005 0.2  --search_lim -0.00005 0.005 --total_lim -0.00005 0.2
 
 
 ### AS ###
 
 DATASET=siemens-as
+OPTIONS="--smooth 10 --landscape --font 20"
 
 # ALL
 echo "bruteforce dciknn flann hnswlib pydci sptag"
--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-DATASET=siemens-as
+DATASETS="siemens-big-sherpa siemens-sherpa siemens-olhc siemens-as"
 ALGORITHMS="bruteforce dciknn flann hnswlib pydci sptag"
 OPTIONS="--timeout 324000 --force --run-disabled"
-for ALG in $ALGORITHMS; do
-	python run.py --dataset $DATASET --count -1 --runs 5 --algorithm $ALG $OPTIONS
+for DATASET in $DATASETS; do
+	for ALG in $ALGORITHMS; do
+		python run.py --dataset $DATASET --count -1 --runs 5 --algorithm $ALG $OPTIONS
+	done
 done
